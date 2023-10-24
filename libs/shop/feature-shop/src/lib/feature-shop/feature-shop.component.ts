b470af555ce24422579a438dc1c7e7933323aadd
Jose Luis Monteagudo <jl_monteagudo@yahoo.es>
2023-10-24 21:38:14 +0200

    [feat] Add breadcrumb component

--- libs/shop/feature-shop/src/lib/feature-shop/feature-shop.component.ts
+++ libs/shop/feature-shop/src/lib/feature-shop/feature-shop.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BreadcrumbComponent } from '../ui/breadcrumb/breadcrumb.component';
 
 @Component({
   selector: 'ecommerce-feature-shop',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './feature-shop.component.html',
-  styleUrls: ['./feature-shop.component.css'],
+  imports: [CommonModule, BreadcrumbComponent],
+  template: ` <ecommerce-breadcrumb /> `,
 })
 export class FeatureShopComponent {}
