24f38cb82dcef37d193c7fc88b08963af6431464
Jose Luis Monteagudo <jl_monteagudo@yahoo.es>
2023-12-25 20:46:53 +0100

    [feat] setup scrollPositionRestoration

--- apps/ecommerce-ng/src/app/app.config.ts
+++ apps/ecommerce-ng/src/app/app.config.ts
@@ -1,10 +1,27 @@
 import { ApplicationConfig } from '@angular/core';
 import {
+  InMemoryScrollingFeature,
+  InMemoryScrollingOptions,
   provideRouter,
   withEnabledBlockingInitialNavigation,
+  withInMemoryScrolling,
 } from '@angular/router';
 import { appRoutes } from './app.routes';
 
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'top',
+  anchorScrolling: 'enabled',
+};
+
+const inMemoryScrollingFeature: InMemoryScrollingFeature =
+  withInMemoryScrolling(scrollConfig);
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+  providers: [
+    provideRouter(
+      appRoutes,
+      withEnabledBlockingInitialNavigation(),
+      inMemoryScrollingFeature
+    ),
+  ],
 };
